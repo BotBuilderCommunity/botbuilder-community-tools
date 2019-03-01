c0a2cd0c4c725ea2313e66132f4ad39ae3445a94
Jamie D <daltskin@hotmail.com>
2019-03-01 13:25:14 +0000

    First commit

---
+++ TalkingBotBridge/ReadMe.md
@@ -0,0 +1,215 @@
+# Talking Bots for Azure Bot Service - get you're bot talking!
+
+Let's face it, making computers talk is always great fun.  So bringing your existing bot to life with voice capability is absolutely necessary if you want to impress your friends/colleagues or manager! 
+This bridge will enable speech input *and* output, so you can leave the keyboard behind :)  However, not only will this perform speech to text (STT) and text to speech (TTS) this will also perform natural language processing for you!
+
+## Pre-reqs
+Don't worry, if you don't have an existing bot, follow the instructions below to get started.  You will need:
+
+- Azure Service bot with direct line enabled
+- LUIS subscription, API key and published model
+- Cognitive Speech subscription and API key
+
+## UWP Desktop Bridge
+
+![Talking Bot Bridge screenshot](./TalkingBotBridge/Docs/screenshot.png)
+
+This is where you can configure your bot, you'll need to grab the `directline` secret from the [Azure Portal](https://portal.azure.com) and paste it here.  Add your bot name (bot handle) as configured in the Azure Bot Service - this is required so that we can determine which messages are from the user and which are from the bot.
+Then you'll need to select which [voice font](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#text-to-speech) to use - here you'll see a range of male/female voices, including our latest neural network trained ones which sound very life-like.  If you want to take this a step further and use your own [Custom Speech voice font](https://westus.cris.ai/Home/CustomSpeech)- simply tweak the code behind for the your own voice font name and endpoint.
+
+## Provision a Sample Bot
+If you don't already have one, the quickest way is to deploy the `Basic Bot` template inside the [Azure Portal](https://portal.azure.com) - this will provision:
+- Azure Service Bot
+- Luis model wired up with configuration
+
+Once you have this, download the bot source code from the [Azure Portal](https://portal.azure.com) and following the [Modifying your bot section](#Modifying-your-bot).
+
+## Speech Intent Recognition
+Here we are using a pretty awesome feature inside the Speech SDK which allows you to do LUIS intent recognition directly from the LUIS service - meaning that not only will it convert the speech input to text, it will also provide the LUIS recognition - returning a json object.  We don't do anything inside this desktop bridge with the LUIS result, we simply pass it onto the bot, so that the bot in turn doesn't need to perform additional LUIS calls to resolve the intent of the user's request - which is a considerable performance benefit.  
+To pass the LUIS response to the bot, we use the `Activity.Value` eg:
+
+#### TalkingBotBridge Extract:
+
+```csharp
+private async Task SendMessageToBot(string messageText, string luisIntent, object luisJson, string activityType)
+{
+    Activity message = new Activity
+    {
+        Text = messageText,
+        Value = luisJson,
+        Type = activityType.ToString(),
+        From = new ChannelAccount(BotUser)
+    };
+
+    await directLineClient.Conversations.PostActivityAsync(botConversation.ConversationId, message);
+}
+```
+
+## Modifying your bot
+
+We're going to make a minor modification to the bot logic so that it doesn't always run LUIS for every call.  Since, if we are coming in via voice then we would already have run the LUIS Intent Recognition.  The [code above](#TalkingBotBridge-Extract) shows you how to pass data through directline and onto the bot, as we're dealing with Json, let's make life a little easier for ourselves my creating a c# backing class we can use for the deserialisation later.
+
+#### LUIS Json Response:
+
+``` csharp
+public class LuisJson
+{
+	public string Query { get; set; }
+
+	public Topscoringintent TopScoringIntent { get; set; }
+
+	public JObject[] Entities { get; set; }
+
+	public class Topscoringintent
+	{
+		public string Intent { get; set; }
+
+		public float Score { get; set; }
+	}
+}
+```
+
+Now, we'll use this class to extract the LUIS data we're interested in within the bot.  The Speech SDK LanguageUnderstandingServiceResponse_JsonResult result is slightly different from the LuisRecognizer LUIS response.  We just want the top intent and the entities that LUIS determined for us.  
+
+Bot Extract:
+
+``` csharp
+if (activity.Type == ActivityTypes.Message)
+{
+	string topIntent;
+	JObject luisEntities;
+	if (activity.Value != null)
+	{
+		var luisResponse = JsonConvert.DeserializeObject<LuisJson>(activity.Value.ToString());
+		topIntent = luisResponse.TopScoringIntent.Intent;
+
+		// Map SpeechRecognizer LanguageUnderstandingServiceResponse_JsonResult) LUIS response to LuisRecognizer
+		luisEntities = new JObject();
+		foreach (var item in luisResponse.Entities)
+		{
+			var p = new JProperty(item["type"].ToString(), new object[] { item["entity"].ToString() });
+			luisEntities.Add(p);
+		}
+	}
+	else
+	{
+		// Perform a call to LUIS to retrieve results for the current activity message.
+		var luisResult = await _services.LuisServices[LuisConfiguration].RecognizeAsync(dc.Context, cancellationToken);
+		var topScoringIntent = luisResult?.GetTopScoringIntent();
+		topIntent = topScoringIntent.Value.intent;
+
+		luisEntities = luisResult.Entities;
+	}
+
+// update greeting state with any entities captured
+await UpdateGreetingState(luisEntities, dc.Context);
+```
+      
+We then change the signature of hte UpdateGreetingState to accept the JObject of the luisEntities only, rather than the complete RecognizerResult, which we don't need anymore. 
+
+```csharp
+private async Task UpdateGreetingState(JObject luisEntities, ITurnContext turnContext)
+{
+	if (luisEntities != null && luisEntities.HasValues)
+	{
+		// Get latest GreetingState
+		var greetingState = await _greetingStateAccessor.GetAsync(turnContext, () => new GreetingState());
+
+		// Supported LUIS Entities
+		string[] userNameEntities = { "userName", "userName_patternAny" };
+		string[] userLocationEntities = { "userLocation", "userLocation_patternAny" };
+
+		// Update any entities
+		// Note: Consider a confirm dialog, instead of just updating.
+		foreach (var name in userNameEntities)
+		{
+			// Check if we found valid slot values in entities returned from LUIS.
+			if (luisEntities[name] != null)
+			{
+				// Capitalize and set new user name.
+				var newName = (string)luisEntities[name][0];
+				greetingState.Name = char.ToUpper(newName[0]) + newName.Substring(1);
+				break;
+			}
+		}
+
+		foreach (var city in userLocationEntities)
+		{
+			if (luisEntities[city] != null)
+			{
+				// Capitalize and set new city.
+				var newCity = (string)luisEntities[city][0];
+				greetingState.City = char.ToUpper(newCity[0]) + newCity.Substring(1);
+				break;
+			}
+		}
+
+		// Set the new values into state.
+		await _greetingStateAccessor.SetAsync(turnContext, greetingState);
+	}
+}
+```
+Once you've made these tweaks, you'll have a bot that can either handle LUIS recognition already done via speech or more regulary, handle the LUIS API service calls itself for text based channels - ensuring that it's only ever performed once for the initial request.
+
+# Cognitive Services
+We're going to be using a combination of the following cognitive services:
+
+- LUIS for natural language understanding
+- Speech for speech to text and text to speech
+
+## LUIS
+Luis allows [Speech Priming](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-manage-speech-priming?view=azure-bot-service-3.0), this is a feature we're going to enable and leverage.  
+
+![Speech Priming](./TalkingBotBridge/Docs/speechpriming.png)
+
+
+
+
+Note, if you're using a starter_key this won't work.  Go and create a LUIS resource in the [Azure Portal](https://portal.azure.com) and assign this to your LUIS model.
+
+## Speech API
+To leverage the [Speech API](https://azure.microsoft.com/en-gb/try/cognitive-services/?api=speech-services) you'll need a key.  Once you've provisioned this in Azure, get the API Key (not Subscription Key). 
+
+![Speech API Key](./TalkingBotBridge/Docs/speechkey.png)
+
+
+Programmatically we'll generate an access token for the Speech API.  To do this we'll need the token endpoint of the API, this will depend on the region you've selected above.  
+
+|   Region   |	Token service endpoint  |
+| ----------------  | --------------------------------------------------------------------- |
+| West US			| https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken |
+| West US2			| https://westus2.api.cognitive.microsoft.com/sts/v1.0/issueToken
+| East US			| https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken
+| East US2			| https://eastus2.api.cognitive.microsoft.com/sts/v1.0/issueToken
+| East Asia			| https://eastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken
+| South East Asia	| https://southeastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken
+| North Europe		| https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issueToken
+| West Europe		| https://westeurope.api.cognitive.microsoft.com/sts/v1.0/issueToken
+
+
+[Full list of speech token endpoints](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-apis)
+
+You'll then need the text to speech endpoint, again this will depend on the region you're using.  We want to use the very latest neural voices, so will choose from one of the following:
+
+|   Region   |	Endpoint  |
+| ----------------  | --------------------------------------------------------------------- |
+| East US			| https://eastus.tts.speech.microsoft.com/cognitiveservices/v1
+| Southeast Asia	| https://southeastasia.tts.speech.microsoft.com/cognitiveservices/v1
+| West Europe		| https://westeurope.tts.speech.microsoft.com/cognitiveservices/v1
+
+
+[Full list of text to speech endpoints](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/regions#text-to-speech)
+
+Once you have the LUIS API key and model ID, together with the Speech API key and endpoints above, set these in the UWP app eg:
+
+```csharp
+private const string STTLanguage = "en-US";
+private const string LuisSubscriptionKey = "<YOUR_LUIS_API_KEY>";
+private const string LuisRegion = "westeurope";
+private const string LuisAppID = "<YOUR_LUIS_APPID>";
+
+private const string TTSSubscriptionKey = "<YOUR_SPEECH_API_KEY>";
+private readonly Uri TTSAPIUri = new Uri("https://<YOUR_REGION>.tts.speech.microsoft.com/cognitiveservices/v1");
+```
+
+Either add your `directline` secret at design/run time, run the app and start talking to your bot!
---
+++ TalkingBotBridge/TalkingBotBridge/MainPage.xaml.cs
@@ -0,0 +1,316 @@
+﻿namespace TalkingBotBridge
+{
+    using Microsoft.Bot.Connector.DirectLine;
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Windows.Media.Core;
+    using Windows.Media.Playback;
+    using Windows.UI.Core;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
+
+    public sealed partial class MainPage : Page
+    {
+        private readonly CoreDispatcher cd;
+        private JamieD.CognitiveSpeech.SpeechToText cognitiveSpeech;
+        private JamieD.CognitiveSpeech.TextToSpeech cognitiveTTS;
+        private DirectLineClient directLineClient;
+        private Conversation botConversation;
+        private bool botIsTalking = false;
+        private bool isRecording = false;
+        private DispatcherTimer mouthTimer;
+
+        // LUIS model and api key
+        private const string STTLanguage = "en-US";
+        private const string LuisSubscriptionKey = "<YOUR_LUIS_API_KEY>";
+        private const string LuisRegion = "westeurope";
+        private const string LuisAppID = "<YOUR_LUIS_APPID>";
+
+        // Text to Speech config
+        // List of token endpoints here: https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-apis
+        // List of text to speech endpoints here: https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/regions#text-to-speech
+        private readonly Uri TTSAPITokenUri = new Uri("https://westeurope.api.cognitive.microsoft.com/sts/v1.0/issueToken");
+        private const string TTSSubscriptionKey = "<YOUR_SPEECH_API_KEY>";
+        private readonly Uri TTSAPIUri = new Uri("https://<YOUR_REGION>.tts.speech.microsoft.com/cognitiveservices/v1");
+
+        // List of formats here: https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-apis
+        private const string TTSOutputFormat = "riff-16khz-16bit-mono-pcm";
+        private const int TTSTokenRefreshDurationMin = 10;
+
+        // Bot config - overridable in UI
+        private string BotDirectLineSecret = "<YOUR_DIRECTLINE_SECRET>";
+        private string BotId = "<YOUR_BOT_HANDLE>";
+        private string BotUser;
+
+        // List of voice fonts here: https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#text-to-speech
+        public string[] voiceFonts = {"(en-US, JessaNeural)", "(en-US, GuyNeural)", "(en-US, JessaRUS)","(en-US, Jessa24kRUS)","(en-US, ZiraRUS)",
+            "(en-US, BenjaminRUS)","(en-US, Guy24kRUS)","(en-GB, Susan, Apollo)","(en-GB, HazelRUS)","(en-GB, George, Apollo)",
+            "(en-IE, Sean)","(en-IN, Heera, Apollo)","(en-IN, Ravi, Apollo)","(en-CA, Linda)"};
+
+        // SAML https://www.w3.org/TR/2009/REC-speech-synthesis-20090303/#edef_prosody
+        public string[] voiceRate = { "default", "x-slow", "slow", "medium", "fast", "x-fast" };
+        public string[] voiceVolume = { "default", "silent", "x-soft", "soft", "medium", "loud", "x-loud" };
+
+        // SAML https://www.w3.org/TR/2009/REC-speech-synthesis-20090303/#pitch_contour
+        public string[] voicePitch = { "default", "x-low", "medium", "high", "x-high" };
+
+        private string watermark = null;
+
+        private readonly Brush TalkingBrushIndicator = new SolidColorBrush(Windows.UI.Colors.Gold);
+        private readonly Brush ListeningBrushIndicator = new SolidColorBrush(Windows.UI.Colors.DarkGray);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            cd = Dispatcher;
+
+            foreach (var font in voiceFonts)
+            {
+                cmbVoiceFont.Items.Add(font);
+            }
+            cmbVoiceFont.SelectedItem = voiceFonts[0];
+
+            foreach (var rate in voiceRate)
+            {
+                cmbVoiceRate.Items.Add(rate);
+            }
+            cmbVoiceRate.SelectedItem = voiceRate[0];
+
+            foreach (var vol in voiceVolume)
+            {
+                cmbVoiceVolume.Items.Add(vol);
+            }
+            cmbVoiceVolume.SelectedItem = voiceVolume[0];
+
+            foreach (var pitch in voicePitch)
+            {
+                cmbVoicePitch.Items.Add(pitch);
+            }
+            cmbVoicePitch.SelectedItem = voicePitch[0];
+
+            txtBotName.Text = BotId;
+            txtDirectLineSecret.Text = BotDirectLineSecret;
+
+            prTalkingStatus.IsActive = false;
+
+            CheckMicSettings();
+            ConfigureServices();
+
+            mouthTimer = new DispatcherTimer();
+            mouthTimer.Interval = TimeSpan.FromMilliseconds(200);
+            mouthTimer.Tick += MouthTimer_Tick;
+            mouthTimer.Start();
+        }
+
+        private async Task CheckMicSettings()
+        {
+            try
+            {
+                var mediaCapture = new Windows.Media.Capture.MediaCapture();
+                var settings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
+                settings.StreamingCaptureMode = Windows.Media.Capture.StreamingCaptureMode.Audio;
+                await mediaCapture.InitializeAsync(settings);
+            }
+            catch (Exception exp)
+            {
+                LogOutput($"Error initialing mic: {exp}");
+            }
+        }
+
+        private void MouthTimer_Tick(object sender, object e)
+        {
+            if (botIsTalking)
+            {
+                LeftEye.Fill = TalkingBrushIndicator;
+                RightEye.Fill = TalkingBrushIndicator;
+                prTalkingStatus.Foreground = TalkingBrushIndicator;
+            }
+            else
+            {
+                LeftEye.Fill = ListeningBrushIndicator;
+                RightEye.Fill = ListeningBrushIndicator;
+                prTalkingStatus.Foreground = ListeningBrushIndicator;
+            }
+        }
+
+        private void ConfigureServices()
+        {
+            cognitiveSpeech = new JamieD.CognitiveSpeech.SpeechToText(STTLanguage, LuisSubscriptionKey, LuisRegion, LuisAppID);
+            cognitiveTTS = new JamieD.CognitiveSpeech.TextToSpeech(TTSAPITokenUri, TTSAPIUri, TTSSubscriptionKey, TTSOutputFormat, TTSTokenRefreshDurationMin);
+            directLineClient = new DirectLineClient(BotDirectLineSecret);
+        }
+
+        private async Task BeginConversation()
+        {
+            BotId = txtBotName.Text;
+            BotDirectLineSecret = txtDirectLineSecret.Text;
+            watermark = null;
+            BotUser = Guid.NewGuid().ToString();
+            botConversation = await directLineClient.Conversations.StartConversationAsync();
+        }
+
+        private async void Talk_Click(object sender, RoutedEventArgs e)
+        {
+            btnTalk.IsEnabled = false;
+            isRecording = true;
+            prTalkingStatus.IsActive = true;
+            try
+            {
+                await Speak("Talk to me!");
+                txtLog.PlaceholderText = "Say something..";
+                await BeginConversation().ConfigureAwait(false);
+                while (isRecording)
+                {
+                    (string luisIntent, string stt, object luisJson) = await cognitiveSpeech.RecognitionWithMicrophoneAsync().ConfigureAwait(false);
+                    stt = stt.RemoveLastChar(".");
+
+                    if (!string.IsNullOrWhiteSpace(luisIntent))
+                    {
+                        LogOutput($"(You): {stt}");
+                        await SendMessageToBot(stt, luisIntent, luisJson, ActivityTypes.Message).ConfigureAwait(false);
+                        watermark = await ReadBotReplyAsync(watermark).ConfigureAwait(false);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                LogOutput($"Error listening: {exp}");
+            }
+        }
+
+        private async void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            btnReset.IsEnabled = false;
+            isRecording = false;
+            txtLog.Text = "";
+            await Task.Delay(2000);
+            prTalkingStatus.IsActive = false;
+            btnTalk.IsEnabled = true;
+            btnReset.IsEnabled = true;
+        }
+
+        private async Task SendMessageToBot(string messageText, string luisIntent, object luisJson, string activityType)
+        {
+            Activity message = new Activity
+            {
+                Text = messageText,
+                Value = luisJson,
+                Type = activityType.ToString(),
+                From = new ChannelAccount(BotUser)
+            };
+
+            await directLineClient.Conversations.PostActivityAsync(botConversation.ConversationId, message);
+        }
+
+        private async Task<string> ReadBotReplyAsync(string watermark)
+        {
+            bool messageReceived = false;
+
+            while (!messageReceived)
+            {
+                try
+                {
+                    var messages = await directLineClient.Conversations.GetActivitiesAsync(botConversation.ConversationId, watermark);
+                    watermark = messages?.Watermark;
+                    var botResponse = messages?.Activities.Where(x => x.From.Name == BotId).ToList();
+                    string textToSay = "";
+                    foreach (var message in botResponse)
+                    {
+                        if (!string.IsNullOrWhiteSpace(message.Speak))
+                        {
+                            textToSay += message.Speak;
+                        }
+                        else if (!string.IsNullOrWhiteSpace(message.Text))
+                        {
+                            textToSay += message.Text;
+                        }
+                        else if (message?.Attachments?.Count > 0)
+                        {
+                            // TODO: HANDLE CARDS
+                        }
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(textToSay))
+                    {
+                        LogOutput($"(Bot): {textToSay}");
+                        await Speak(textToSay);
+                    }
+                    messageReceived = true;
+                }
+                catch (Exception exp)
+                {
+                    LogOutput($"Error contacting bot: {exp}");
+                }
+
+            }
+            return watermark;
+        }
+
+        private async Task Speak(string text)
+        {
+            try
+            {
+                string fontName = "";
+                string rate = "";
+                string volume = "";
+                string pitch = "";
+
+                await cd.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    fontName = $"Microsoft Server Speech Text to Speech Voice {cmbVoiceFont.SelectedItem}";
+                    rate = cmbVoiceRate.SelectedItem.ToString();
+                    volume = cmbVoiceVolume.SelectedItem.ToString();
+                    pitch = cmbVoicePitch.SelectedItem.ToString();
+                });
+
+                using (var speechOutput = await cognitiveTTS.GetAudioStream(
+                        text: text,
+                        voiceFont: fontName,
+                        rate: rate,
+                        volume: volume,
+                        pitch: pitch))
+                {
+                    await PlaySpeechOutput(speechOutput);
+                }
+            }
+            catch (Exception exp)
+            {
+                LogOutput($"Error speaking {exp}");
+            }
+        }
+
+        private async Task PlaySpeechOutput(MemoryStream speechOutput)
+        {
+            if (speechOutput != null && speechOutput.Length > 0)
+            {
+                var taskSource = new TaskCompletionSource<object>();
+                using (MediaPlayer mp = new MediaPlayer())
+                {
+                    mp.MediaEnded += (s, e) =>
+                    {
+                        botIsTalking = false;
+                        taskSource.SetResult(null);
+                    };
+                    mp.Source = MediaSource.CreateFromStream(speechOutput.AsRandomAccessStream(), "audio/mpeg");
+                    mp.Play();
+                    botIsTalking = true;
+                    await taskSource.Task;
+                }
+            }
+        }
+
+        private async void LogOutput(string text)
+        {
+            await cd.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                txtLog.Text = txtLog.Text + Environment.NewLine + text;
+                txtLog.ScrollToBottom();
+            });
+            Debug.WriteLine(text);
+        }
+    }
+}
