07e84d95b5875862493a62ff7ecdd9a935a7352d
rvinothrajendran <r.vinoth@live.com>
2021-10-14 06:20:15 +0200

    Added Bot custom action template for VS2022

---
+++ VSProjectTemplate/BotComposerCustomActionTemplate/TemplateCode/BotCustomAction/BotCustomAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+
+namespace BotCustomAction
+{
+    //DescriptionContent
+    public class BotCustomAction : Dialog
+    {
+        public BotCustomAction([CallerFilePath] string sourceFilePath = "",[CallerLineNumber] int sourceLineNumber=0) : base()
+        {
+            RegisterSourceLocation(sourceFilePath,sourceLineNumber);
+        }
+        
+        [JsonProperty("$Kind")]
+        public const string Kind = "BotCustomAction";
+
+        [JsonProperty("resultProperty")]
+        public StringExpression ResultProperty { get; set; }
+
+        public override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            var result = string.Empty;
+
+            if (ResultProperty != null)
+            {
+                dc.State.SetValue(this.ResultProperty.GetValue(dc.State),result);
+            }
+
+            return dc.EndDialogAsync(result: result, cancellationToken:cancellationToken);
+        }
+    }
+}
