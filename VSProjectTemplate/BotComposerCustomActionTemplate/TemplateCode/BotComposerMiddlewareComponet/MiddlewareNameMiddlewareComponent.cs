bbfa6991735cc0fd75c1144199629e0d6a8c0f89
rvinothrajendran <r.vinoth@live.com>
2021-10-22 19:17:40 +0200

    Added Middleware template

---
+++ VSProjectTemplate/BotComposerCustomActionTemplate/TemplateCode/BotComposerMiddlewareComponet/MiddlewareNameMiddlewareComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BotComposerMiddlewareComponent.Middleware;
+using Microsoft.Bot.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BotComposerMiddlewareComponent
+{
+    public class MiddlewareNameMiddlewareComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            
+            if(services == null)
+                throw new ArgumentNullException(nameof(services));  
+
+            if(configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+			services.AddSingleton<IMiddleware, MiddlewareNameMiddleware>();
+
+        }
+    }
+}
